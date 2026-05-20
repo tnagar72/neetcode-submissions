bed378aa6e4b4de38d57b1eac45285cbef877e7b
Tanay Nagar <90069326+tnagar72@users.noreply.github.com>
2026-05-20 16:44:19 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freq_list = [0] * 26
+        freq_count = {}
+
+        for s in strs:
+            for char in s:
+                freq_list[ord(char) - ord('a')] += 1
+            freq_tuple = tuple(freq_list)
+
+            if freq_tuple not in freq_count.keys():
+                freq_count[freq_tuple] = list()
+                freq_count[freq_tuple].append(s)
+            else:
+                freq_count[freq_tuple].append(s)
+            freq_list = [0] * 26
+
+        
+        # recreate list
+
+        res = []
+        for key, value in freq_count.items():
+            res.append(value)
+        
+        return res
